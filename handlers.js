56721cba2323ed81daa86d2ebcf056cdbeb91e29
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-28 14:48:29 +0530

    handling A button press (for xbox controllers)

--- handlers.js
+++ handlers.js
@@ -6,6 +6,7 @@ let currentElement = {
 };
 let holdStatus = {};
 
+// TODO add idle thresholds to prevent from ghost scrolling
 function handleRightThumbstick( horizontalAxis, verticalAxis, controller ) {
 	if ( verticalAxis > 0.2 || verticalAxis < 0.2 ) {
 		let m = controller.buttons[11].pressed ? scrollMultiplier * sprintScrollMultiplier : scrollMultiplier;
@@ -14,6 +15,7 @@ function handleRightThumbstick( horizontalAxis, verticalAxis, controller ) {
 }
 
 function handleLeftThumbstick( horizontalAxis, verticalAxis, controller ) {
+	// handle infinite polling
 	switch( horizontalAxis ) {
 		case 1:
 			if ( holdStatus.leftThumbstick == "right" ) {
@@ -43,4 +45,45 @@ function handleLeftThumbstick( horizontalAxis, verticalAxis, controller ) {
 	deAnimateSelector( previous, 5 );
 	currentElement.element = document.getElementsByClassName( "style-scope ytd-grid-renderer" )[currentElement.index];
 	animateSelector( currentElement.element, 5 );
+}
+
+// only fires when A button pressed (only for xbox controllers)
+function handleAButton( controller ) {
+	click( currentElement.element );
+}
+
+function animateSelector( element, width ) {
+	if ( !element ) {
+		return;
+	}
+
+	for ( let curWidth = 0; curWidth < width; curWidth++ ) {
+		setTimeout( function() {
+			element.style.outline = curWidth + "px solid red";
+		}, 20 * curWidth );
+	}
+}
+
+function deAnimateSelector( element, width ) {
+	if ( !element ) {
+		return;
+	}
+
+	let count = 0;
+	for ( let curWidth = width; curWidth >= 0; curWidth-- ) {
+		setTimeout( function() {
+			element.style.outline = curWidth + "px solid red";
+		}, 20 * count );
+		count++;
+	}
+}
+
+function click( element ) {
+	element.childNodes.forEach( function( node ) {
+		if ( node.id == ( "video-title" ) ) {
+			window.open( node.href, "_self" );
+		} else {
+			click( node );
+		}
+	} );
 }
