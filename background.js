9150efe440358ae60c8c59823d4e05a15f30d274
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-30 01:40:50 +0530

    adding listener for handling zoom events

--- background.js
+++ background.js
@@ -29,7 +29,25 @@ chrome.runtime.onMessage.addListener( function( message, sender, sendResponse ) 
 		lastInputs.selectButtonLast = message.current;
 		return;
 	}
+
+	if ( type === "startbuttonpoll" ) {
+		if ( message.current == 1 && lastInputs.startButtonLast != message.current ) {
+			openNewTab();
+		}
+
+		lastInputs.startButtonLast = message.current;
+		return;
+	}
+
+	if ( type === "requestzoom" ) {
+		effectCurrent( function( tab, index, id ) {
+			chrome.tabs.getZoom( id, function( zoomFactor ) {
+				chrome.tabs.setZoom( id, zoomFactor + message.factor / 50, function() {} )
+			} );
+		} );
+	}
 } );
+
 
 function moveTabBy( amount ) {
 	chrome.tabs.query( {}, function( s ) {
@@ -54,6 +72,13 @@ function closeCurrentTab() {
 	} );
 }
 
+function openNewTab() {
+	chrome.tabs.create( {
+			url: "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjnqufjjLTaAhVpx1QKHf2BDSQQPAgD"},
+			function() {}
+		);
+}
+
 function effectCurrent( func ) {
 	chrome.tabs.query( {}, function( s ) {
 		let activeIndex = -1;
@@ -66,6 +91,7 @@ function effectCurrent( func ) {
 		}
 
 		if ( activeIndex >= 0 ) {
+			// tab, tab index, tab id
 			func( s[activeIndex], activeIndex, s[activeIndex].id );
 		}
 	} );
