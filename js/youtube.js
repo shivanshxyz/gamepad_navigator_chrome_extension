2caa4157198730d4475203029b2080aa5e27df08
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-31 02:12:50 +0530

    Adding youtube controls

---
+++ js/youtube.js
@@ -0,0 +1,107 @@
+var selectedVideoIndex = -1;
+
+// clicking on the video using A Button
+window.addEventListener( "abuttonpressed", function() {
+	if ( selectedVideoIndex < 0 ) {
+		return;
+	}
+
+	window.location.href = $( "ytd-thumbnail" ).get( selectedVideoIndex ).children[0].href;
+	deselectAll();
+} );
+
+// Horizontal navigation for available videos
+window.addEventListener( "leftanaloghorizontalmax", function( e ) {
+	let curr = e.detail.current;
+	let prev = e.detail.previous;
+	let ct;
+
+	if ( prev == curr ) {
+		return;
+	}
+
+	if ( curr >= 1 ) {
+		selectVideo( selectedVideoIndex + 1 );
+	} else if ( curr <= -1 ) {
+		selectVideo( selectedVideoIndex - 1 );
+	}
+} );
+
+// Vertical navigation for available videos
+window.addEventListener( "leftanalogverticalmax", function( e ) {
+	let curr = e.detail.current;
+	let prev = e.detail.previous;
+	let ct;
+	if ( selectedVideoIndex != -1 ) {
+		ct = $( "ytd-thumbnail" ).get( selectedVideoIndex ).parentElement.parentElement.parentElement;
+	}
+
+	let changeBy = 1;
+
+	if ( ct && ct.id == "items" ) {
+		changeBy = Math.floor( ct.offsetWidth / 210 );
+	}
+
+	if ( prev == curr ) {
+		return;
+	}
+
+
+	if ( curr >= 1 ) {
+		selectVideo( selectedVideoIndex + changeBy );
+	} else if ( curr <= -1 ) {
+		selectVideo( selectedVideoIndex - changeBy );
+	}
+} );
+
+
+function scrollToVisible( elm ) {
+	if ( !elm ) {
+		return;
+	}
+
+	let topBarHeight = $( "#container" ).height();
+
+	let rect = elm.getBoundingClientRect();
+	let viewHeight = Math.max( document.documentElement.clientHeight, window.innerHeight );
+	  
+	// Get how much off screen it is
+	let bOff = rect.bottom - viewHeight;
+	let tOff = rect.top - topBarHeight;
+
+	if ( bOff > 0 ) {
+		window.scrollBy( 0, bOff + 50 );
+	} else if ( tOff < 0 ) {
+		window.scrollBy( 0, tOff - rect.height + 40 );
+	}
+}
+
+function selectVideo( toSelect ) {
+	let thumbnail = $( "ytd-thumbnail" ).get( toSelect );
+
+	if ( !thumbnail ) {
+		return;
+	}
+
+	scrollToVisible( thumbnail );
+	thumbnail.style["border-style"] = "solid";
+	thumbnail.style["border-color"] = "red";
+	thumbnail.style["border-width"] = "2px";
+
+	deselectVideo( selectedVideoIndex );
+	selectedVideoIndex = toSelect;
+}
+
+function deselectVideo( toDeselect ) {
+	let thumbnail = $( "ytd-thumbnail" ).get( toDeselect );
+	thumbnail.style["border-style"] = "none";
+}
+
+function deselectAll() {
+	let thumbnails = $( "ytd-thumbnail" );
+	selectedVideoIndex = -1;
+
+	for ( let elem in thumbnails ) {
+		thumbnails[elem].style["border-style"] = "none";
+	}
+}
