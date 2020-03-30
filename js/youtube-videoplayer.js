095b5b4b980bff8c10d5651babd58e3e777f4032
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-31 02:14:18 +0530

    Adding youtube video player controls

---
+++ js/youtube-videoplayer.js
@@ -0,0 +1,24 @@
+// X pauses/plays the video
+window.addEventListener( "xbuttonpressed", function() {
+	let playButton = $( ".ytp-play-button.ytp-button" );
+	playButton.trigger( "click" );
+} );
+
+// B makeshift zoom
+window.addEventListener( "bbuttonpressed", function() {
+	let fs = $( ".ytp-size-button" );
+	fs.trigger( "click" );
+} );
+
+// holding Y displays the bar at the bottom
+window.addEventListener( "ybuttonpoll", function() {
+	let vc = $( "div .html5-video-player" );
+	vc.removeClass( "ytp-autohide" );
+} );
+
+window.addEventListener( "ybuttonreleased", function() {
+	let vc = $( "div .html5-video-player" );
+	if ( !vc.hasClass( "ytp-autohide" ) ) {
+		vc.addClass( "ytp-autohide" );
+	}
+} );
